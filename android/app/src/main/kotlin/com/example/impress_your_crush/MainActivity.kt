6bb6776f55c69c577075743fbe8074e5824d20c2
vaidehi2701 <vaidehi2701@gmail.com>
2023-05-19 00:20:46 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/impress_your_crush/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.impress_your_crush
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Focus Item Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<String> imageList = [
+    'assets/images/a1.jpg',
+    'assets/images/a2.jpg',
+    'assets/images/a3.jpg',
+    'assets/images/a4.png',
+    'assets/images/a5.jpg',
+  ];
+
+   MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        title: const Text('Focus Item Animation'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 70),
+        child: SizedBox(
+          height: 150,
+          child: ListView.separated(
+            separatorBuilder: (context, index) {
+              return const SizedBox(
+                width: 5,
+              );
+            },
+            itemCount: imageList.length,
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.only(left: index == 0 ? 20 : 0, right: 10),
+                child: FocusItem(
+                  imageAsset: imageList[index],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FocusItem extends StatefulWidget {
+  final String imageAsset;
+
+  const FocusItem({Key? key, required this.imageAsset}) : super(key: key);
+
+  @override
+  _FocusItemState createState() => _FocusItemState();
+}
+
+class _FocusItemState extends State<FocusItem> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() {
+          isSelected = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          isSelected = false;
+        });
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: AnimatedOpacity(
+          duration: const Duration(milliseconds: 200),
+          opacity: isSelected ? 1.0 : 0.2,
+          child: Image.asset(
+            widget.imageAsset,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
